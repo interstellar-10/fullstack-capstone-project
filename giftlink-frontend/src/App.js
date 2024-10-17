9a026d8203c28f0a57abc8f65b22453252aced75
interstellar-10 <hamdan.almansoori@protected.ae>
2024-10-17 07:17:51 -0400

    login and register pages

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -4,8 +4,12 @@ import MainPage from "./components/MainPage/MainPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import LoginPage from "./components/LoginPage/LoginPage";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <>
       <Navbar />
@@ -13,6 +17,8 @@ function App() {
         {/* the final code will not pass the products to every page, but each page will call the server API */}
         <Route path="/" element={<MainPage />} />
         <Route path="/app" element={<MainPage />} />
+        <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/register" element={<RegisterPage />} />
       </Routes>
     </>
   );

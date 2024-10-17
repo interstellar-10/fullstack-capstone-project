8ea11da0d849e2e444a074ee58e0c22b2f0d5e5a
interstellar-10 <hamdan.almansoori@protected.ae>
2024-10-17 06:24:38 -0400

    main page and nav bar

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -4,7 +4,7 @@ const connectToDatabase = require('../models/db');
 const logger = require('../logger');
 
 // Get all gifts
-router.get('/', async ( res, next) => {
+router.get('/', async (req, res, next) => {
     logger.info('/ called');
     try {
         const db = await connectToDatabase();
@@ -13,7 +13,7 @@ router.get('/', async ( res, next) => {
         const gifts = await collection.find({}).toArray();
         res.json(gifts);
     } catch (e) {
-        logger.console.error('oops something went wrong', e)
+        logger.error('oops something went wrong', e)
         next(e);
     }
 });

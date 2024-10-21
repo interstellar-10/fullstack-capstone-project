79e33c58c41eee027806bc63779195f05d049450
interstellar-10 <hamdan.almansoori@protected.ae>
2024-10-21 06:31:13 -0400

    login auth

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,70 +1,122 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { urlConfig } from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 import "./LoginPage.css";
 
 function LoginPage() {
-  //insert code here to create useState hook variables for email, password
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    //insert code here to create useState hook variables for email, password
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [incorrect, setIncorrect] = useState('');
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
 
-  // insert code here to create handleLogin function and include console.log
-  const handleLogin = async () => {
-    console.log("inside handelLogin");
-  };
+    useEffect(() => {
 
-  return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-md-6 col-lg-4">
-          <div className="login-card p-4 border rounded">
-            <h2 className="text-center mb-4 font-weight-bold">Login</h2>
+        if (sessionStorage.getItem('auth-token')) {
+            navigate('/app');
+        }
+    }, [navigate]);
 
-            {/* insert code here to create input elements for the variables email and  password */}
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">
-                Email
-              </label>
-              <input
-                id="email"
-                type="text"
-                className="form-control"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
+    // insert code here to create handleLogin function and include console.log
+    const handleLogin = async (e) => {
+        try {
+            const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+                //Step 1 - Task 6
+                method: 'POST',
+                //Step 1 - Task 7
+                headers: {
+                    'content-type': 'application/json',
+                    'Authorization': bearerToken ? `Bearer ${bearerToken}` : ' ',
+                },
+                //Step 1 - Task 8
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                })
+            });
+            const json = await res.json();
+            if (json.authtoken) {
+
+                sessionStorage.setItem('auth-token', json.authtoken);
+                sessionStorage.setItem('name', json.userName);
+                sessionStorage.setItem('email', json.userEmail);
+
+                setIsLoggedIn(true);
+                navigate('/app');
+            }
+            else {
+                document.getElementById('email').value = '';
+                document.getElementById('password').value = '';
+                setIncorrect("Wrong password! PLZ Try again.");
+                setTimeout(() => {
+                    setIncorrect('');
+                }, 2000);
+            }
+        } catch (e) {
+            console.log("Error fetching details: " + e.message);
+        }
+
+    };
+
+    return (
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6 col-lg-4">
+                    <div className="login-card p-4 border rounded">
+                        <h2 className="text-center mb-4 font-weight-bold">Login</h2>
+
+                        {/* insert code here to create input elements for the variables email and  password */}
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">
+                                Email
+                            </label>
+                            <input
+                                id="email"
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter your email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form label">
+                                Password
+                            </label>
+                            <br />
+                            <input
+                                id="password"
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter your password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <span style={{ color: 'red', height: '.5cm', display: 'block', fontStyle: 'italic', fontSize: '12px' }}>{incorrect}</span>
+                        </div>
+
+
+                        {/* insert code here to create a button that performs the `handleLogin` function on click */}
+                        <button
+                            className="btn btn-primary w-100 mb-3"
+                            onClick={handleLogin}
+                        >
+                            Login
+                        </button>
+                        <p className="mt-4 text-center">
+                            New here?{" "}
+                            <a href="/app/register" className="text-primary">
+                                Register Here
+                            </a>
+                        </p>
+                    </div>
+                </div>
             </div>
-            <div className="mb-3">
-              <label htmlFor="password" className="form label">
-                Password
-              </label>
-              <br />
-              <input
-                id="password"
-                type="text"
-                className="form-control"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-
-            {/* insert code here to create a button that performs the `handleLogin` function on click */}
-            <button
-              className="btn btn-primary w-100 mb-3"
-              onClick={handleLogin}
-            >
-              Login
-            </button>
-            <p className="mt-4 text-center">
-              New here?{" "}
-              <a href="/app/register" className="text-primary">
-                Register Here
-              </a>
-            </p>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default LoginPage;

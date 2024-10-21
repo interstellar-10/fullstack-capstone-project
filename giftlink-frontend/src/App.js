2bf27d77d119ab6a1ee494eadfc8de4114b7f879
interstellar-10 <hamdan.almansoori@protected.ae>
2024-10-21 07:25:30 -0400

    user profile

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -8,24 +8,26 @@ import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
 import DetailsPage from "./components/DetailsPage/DetailsPage";
 import SearchPage from "./components/SearchPage/SearchPage";
+import Profile from "./components/Profile/Profile";
 
 function App() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        {/* the final code will not pass the products to every page, but each page will call the server API */}
-        <Route path="/" element={<MainPage />} />
-        <Route path="/app" element={<MainPage />} />
-        <Route path="/app/login" element={<LoginPage />} />
-        <Route path="/app/register" element={<RegisterPage />} />
-        <Route path="/app/product/:productId" element={<DetailsPage />} />
-        <Route path="/app/search" element={<SearchPage />} />
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                {/* the final code will not pass the products to every page, but each page will call the server API */}
+                <Route path="/" element={<MainPage />} />
+                <Route path="/app" element={<MainPage />} />
+                <Route path="/app/login" element={<LoginPage />} />
+                <Route path="/app/register" element={<RegisterPage />} />
+                <Route path="/app/product/:productId" element={<DetailsPage />} />
+                <Route path="/app/search" element={<SearchPage />} />
+                <Route path="/app/profile" element={<Profile />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
